80601af4212776c83d06a66f695c35656aacb27e
Xuan Wang <wangxuan.buaa@gmail.com>
2016-07-21 16:04:17 -0700

    Initial commit

---
+++ AmusementParkPassGenerator-Pt2/EntrantModel.swift
@@ -0,0 +1,388 @@
+//
+//  EntrantModel.swift
+//  AmusementParkPassGenerator-Pt2
+//
+//  Created by XuanWang on 7/21/16.
+//  Copyright © 2016 XuanWang. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Protocols
+
+protocol AreaAccess {
+    //var areaAccess: AreaAccessType  { get }
+    func getAreaAccessDetail() -> AreaAccessType
+}
+
+protocol RideAccess {
+    //var rideAccess: RideAccessType { get }
+    func getRideAccessDetail() -> RideAccessType
+}
+
+protocol DiscountAccess {
+    //var discountAccess: DiscountAccessType  { get }
+    func getDiscountAccessDetail() -> DiscountAccessType
+}
+
+protocol Entrant: AreaAccess, RideAccess, DiscountAccess {
+    
+}
+
+// MARK: Access types
+
+struct AreaAccessType {
+    var amusement: Bool
+    var kitchen: Bool
+    var rideControl: Bool
+    var maintenance: Bool
+    var office: Bool
+}
+
+struct RideAccessType {
+    var accessAllRides: Bool
+    var skipAllRideLines: Bool
+}
+
+struct DiscountAccessType {
+    var food: Int
+    var merchandise: Int
+}
+
+// MARK: Entrant types
+
+enum Guest: Entrant {
+    case classic
+    case vip
+    case freeChild
+    case seasonPass
+    case senior
+    
+    func getAreaAccessDetail() -> AreaAccessType {
+        return AreaAccessType(amusement: true, kitchen: false, rideControl: false, maintenance: false, office: false)
+    }
+    
+    func getRideAccessDetail() -> RideAccessType {
+        var skipAllRideLines: Bool
+        switch self {
+        case .classic, .freeChild:
+            skipAllRideLines = false
+        case .vip, .seasonPass, .senior:
+            skipAllRideLines =  true
+        }
+        return RideAccessType(accessAllRides: true, skipAllRideLines: skipAllRideLines)
+    }
+    
+    func getDiscountAccessDetail() -> DiscountAccessType {
+        var foodDiscount: Int
+        var merchandiseDiscount: Int
+        switch self {
+        case .vip, .seasonPass:
+            foodDiscount = 10
+            merchandiseDiscount = 20
+        case .senior:
+            foodDiscount = 10
+            merchandiseDiscount = 10
+        default:
+            foodDiscount = 0
+            merchandiseDiscount = 0
+        }
+        return DiscountAccessType(food: foodDiscount, merchandise: merchandiseDiscount)
+    }
+}
+
+
+enum Employee: Entrant {
+    case hourlyFood
+    case hourlyRide
+    case hourlyMaintenance
+    
+    func getAreaAccessDetail() -> AreaAccessType {
+        var kitchenAccess: Bool, rideControlAccess: Bool, maintenanceAccess: Bool, officeAccess: Bool
+        switch self {
+        case .hourlyFood:
+            kitchenAccess = true
+            rideControlAccess = false
+            maintenanceAccess = false
+            officeAccess = false
+        case .hourlyRide:
+            kitchenAccess = false
+            rideControlAccess = true
+            maintenanceAccess = false
+            officeAccess = false
+        case .hourlyMaintenance:
+            kitchenAccess = true
+            rideControlAccess = true
+            maintenanceAccess = true
+            officeAccess = false
+        }
+        return AreaAccessType(amusement: true, kitchen: kitchenAccess, rideControl: rideControlAccess, maintenance: maintenanceAccess, office: officeAccess)
+    }
+    
+    func getRideAccessDetail() -> RideAccessType {
+        return RideAccessType(accessAllRides: true, skipAllRideLines: false)
+    }
+    
+    func getDiscountAccessDetail() -> DiscountAccessType {
+        return DiscountAccessType(food: 15, merchandise: 25)
+    }
+}
+
+enum Manager: Entrant {
+    case manager
+    
+    func getAreaAccessDetail() -> AreaAccessType {
+        return AreaAccessType(amusement: true, kitchen: true, rideControl: true, maintenance: true, office: true)
+    }
+    
+    func getRideAccessDetail() -> RideAccessType {
+        return RideAccessType(accessAllRides: true, skipAllRideLines: false)
+    }
+    
+    func getDiscountAccessDetail() -> DiscountAccessType {
+        return DiscountAccessType(food: 25, merchandise: 25)
+    }
+}
+
+// MARK: Other objects
+
+struct PersonalInfo {
+    var firstName: String?
+    var lastName: String?
+    var dateOfBirth: String?
+    var street: String?
+    var city: String?
+    var state: String?
+    var zip: String?
+    
+}
+
+struct Pass {
+    let title: String
+    let passType: String
+    let rideInfo: String
+    let foodDiscountInfo: String
+    let merchandiseDiscountInfo: String
+    let areaAccess: AreaAccessType
+    let rideAccess: RideAccessType
+    let discountAccess: DiscountAccessType
+    let personalInfo: PersonalInfo
+}
+
+// MARK: Error Types
+
+enum RequiredInfoError: ErrorType {
+    case MissingFirstName
+    case MissingLastName
+    case MissingStreetAddress
+    case MissingCity
+    case MissingState
+    case MissingZipCode
+    case MissingDateOfBirth
+    case AgeRequirementNotMet
+    case DateFormatNotCorrect
+}
+
+
+// MARK: Generate Pass
+
+func printPass(entrantType: Entrant, person: PersonalInfo) {
+    if let pass = generatePass(entrantType, person: person) {
+        print(pass.title + "\n" + pass.passType + "\n" + pass.rideInfo + "\n" + pass.foodDiscountInfo + "\n" + pass.merchandiseDiscountInfo)
+    }
+}
+
+func generatePass(entrantType: Entrant, person: PersonalInfo) -> Pass? {
+    let title: String
+    let passType: String
+    let rideInfo: String
+    let foodDiscountInfo: String
+    let merchandiseDiscountInfo: String
+    let areaAccess: AreaAccessType
+    let rideAccess: RideAccessType
+    let discountAccess: DiscountAccessType
+    
+    do {
+        let infoGathered = try gatherRequiredInfo(entrantType, person: person)
+        if let firstName = infoGathered.firstName, lastName = infoGathered.lastName {
+            title = "\(firstName) \(lastName)"
+        } else {
+            title = "Amusement Park Pass"
+        }
+        
+        areaAccess = entrantType.getAreaAccessDetail()
+        rideAccess = entrantType.getRideAccessDetail()
+        discountAccess = entrantType.getDiscountAccessDetail()
+        
+        foodDiscountInfo = "\(discountAccess.food)% food discount."
+        merchandiseDiscountInfo = "\(discountAccess.merchandise)% merchandise discount."
+        
+        if rideAccess.accessAllRides == true {
+            if rideAccess.skipAllRideLines == true {
+                rideInfo = "Unlimited Rides. Priority: VIP."
+            } else {
+                rideInfo = "Unlimited Rides. Priority: Regular."
+            }
+        } else {
+            rideInfo = "No access to rides."
+        }
+        
+        switch entrantType {
+        case is Guest:
+            let guestType = entrantType as! Guest
+            switch guestType {
+            case .classic:
+                passType = "Classic Guest Pass"
+            case .freeChild:
+                passType = "Child Guest Pass"
+            case .vip:
+                passType = "VIP Guest Pass"
+            case .seasonPass:
+                passType = "Season Guest Pass"
+            case .senior:
+                passType = "Senior Guest Pass"
+            }
+            
+        case is Manager:
+            passType = "Manager"
+            
+        default:
+            let employeeType = entrantType as! Employee
+            switch employeeType {
+            case .hourlyFood:
+                passType = "Hourly Employee - Food Services"
+            case .hourlyRide:
+                passType = "Hourly Employee - Ride Services"
+            case .hourlyMaintenance:
+                passType = "Hourly Employee - Maintenance"
+            }
+        }
+        
+        return Pass(title: title, passType: passType, rideInfo: rideInfo, foodDiscountInfo: foodDiscountInfo, merchandiseDiscountInfo: merchandiseDiscountInfo, areaAccess: areaAccess, rideAccess: rideAccess, discountAccess: discountAccess, personalInfo: infoGathered)
+        
+    } catch let error {
+        print("Error: \(error)")
+        return nil
+    }
+}
+
+func gatherRequiredInfo(entrantType: Entrant, person: PersonalInfo) throws -> PersonalInfo {
+    switch entrantType {
+    case is Guest:
+        let guestType = entrantType as! Guest
+        switch guestType {
+        case .freeChild:
+            if let dateOfBirth = person.dateOfBirth {
+                if satisfyAgeRequirement(dateOfBirth) {
+                    return person
+                } else {
+                    throw RequiredInfoError.AgeRequirementNotMet
+                }
+            } else {
+                throw RequiredInfoError.MissingDateOfBirth
+            }
+        case .seasonPass:
+            guard person.firstName != nil else {
+                throw RequiredInfoError.MissingFirstName
+            }
+            guard person.lastName != nil else {
+                throw RequiredInfoError.MissingLastName
+            }
+            guard person.street != nil else {
+                throw RequiredInfoError.MissingStreetAddress
+            }
+            guard person.city != nil else {
+                throw RequiredInfoError.MissingCity
+            }
+            guard person.state != nil else {
+                throw RequiredInfoError.MissingState
+            }
+            guard person.zip != nil else {
+                throw RequiredInfoError.MissingZipCode
+            }
+            return person
+        case .senior:
+            guard person.firstName != nil else {
+                throw RequiredInfoError.MissingFirstName
+            }
+            guard person.lastName != nil else {
+                throw RequiredInfoError.MissingLastName
+            }
+            guard person.dateOfBirth != nil else {
+                throw RequiredInfoError.MissingDateOfBirth
+            }
+            return person
+        default:
+            return person
+        }
+    default:
+        // Default will be employee or manager type, require all info except date of birth
+        guard person.firstName != nil else {
+            throw RequiredInfoError.MissingFirstName
+        }
+        guard person.lastName != nil else {
+            throw RequiredInfoError.MissingLastName
+        }
+        guard person.street != nil else {
+            throw RequiredInfoError.MissingStreetAddress
+        }
+        guard person.city != nil else {
+            throw RequiredInfoError.MissingCity
+        }
+        guard person.state != nil else {
+            throw RequiredInfoError.MissingState
+        }
+        guard person.zip != nil else {
+            throw RequiredInfoError.MissingZipCode
+        }
+        return person
+    }
+}
+
+
+
+// Helper Methods
+
+func convertStringToNSDate(dateOfBirthAsString: String) throws -> NSDate? {
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "MM/dd/yyyy"
+    if let dateOfBirthAsNSDate: NSDate = dateFormatter.dateFromString(dateOfBirthAsString){
+        return dateOfBirthAsNSDate
+    } else {
+        throw RequiredInfoError.DateFormatNotCorrect
+    }
+}
+
+func satisfyAgeRequirement(dateOfBirthAsString: String) -> Bool {
+    do {
+        let currentDate = NSDate()
+        if let dateOfBirth = try convertStringToNSDate(dateOfBirthAsString) {
+            let diffDateComponents = NSCalendar.currentCalendar().components(NSCalendarUnit.Year, fromDate: dateOfBirth, toDate: currentDate, options: NSCalendarOptions.init(rawValue: 0))
+            if diffDateComponents.year < 5 {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    catch let error {
+        print("Error: \(error)")
+    }
+    return false
+}
+
+func checkBirthday(dateOfBirthAsString: String) {
+    do {
+        let currentDate = NSDate()
+        if let dateOfBirth = try convertStringToNSDate(dateOfBirthAsString) {
+            let birthdayDateComponents = NSCalendar.currentCalendar().components([NSCalendarUnit.Month, NSCalendarUnit.Day], fromDate: dateOfBirth)
+            let currentDayDateComponents = NSCalendar.currentCalendar().components([NSCalendarUnit.Month, NSCalendarUnit.Day], fromDate: currentDate)
+            if birthdayDateComponents.month == currentDayDateComponents.month && birthdayDateComponents.day == currentDayDateComponents.day {
+                print("Happy birthday! Hope you enjoy your time here!")
+            }
+        }
+    }
+    catch let error {
+        print("Error: \(error)")
+    }
+}
--- AmusementParkPassGenerator-Pt2/ViewController.swift
+++ AmusementParkPassGenerator-Pt2/ViewController.swift
@@ -13,6 +13,78 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Plug data for pass generation testing
+        //
+        //        let plugClassicGuest = PersonalInfo(firstName: nil, lastName: nil, dateOfBirth: nil, street: nil, city: nil, state: nil, zip: nil)
+        //        printPass(Guest.classic, person: plugClassicGuest)
+        //        print("\n")
+        //
+        //        let plugVIPGuest = PersonalInfo(firstName: "Ted", lastName: "Mosby", dateOfBirth: nil, street: nil, city: nil, state: nil, zip: nil)
+        //        printPass(Guest.vip, person: plugVIPGuest)
+        //        print("\n")
+        //
+        //        let plugChildGuest = PersonalInfo(firstName: nil, lastName: nil, dateOfBirth: "03/02/2012", street: nil, city: nil, state: nil, zip: nil)
+        //        printPass(Guest.freeChild, person: plugChildGuest)
+        //        print("\n")
+        //
+        //        let plugHourlyEmployeeFood = PersonalInfo(firstName: "David", lastName: "Parker", dateOfBirth: nil, street: "412 Whisman Rd", city: "Mountain View", state: "CA", zip: "94023")
+        //        printPass(Employee.hourlyFood, person: plugHourlyEmployeeFood)
+        //        print("\n")
+        //
+        //        let plugHourlyEmployeeRide = PersonalInfo(firstName: "Matt", lastName: "Monohan", dateOfBirth: nil, street: "329 Candy Rd", city: "Los Angeles", state: "CA", zip: "83923")
+        //        printPass(Employee.hourlyRide, person: plugHourlyEmployeeRide)
+        //        print("\n")
+        //
+        //        let plugHourlyEmployeeMaintenance = PersonalInfo(firstName: "Charlie", lastName: "Brown", dateOfBirth: "07-03-1986", street: "920 Peanut Street", city: "Chicago", state: "IL", zip: "37283")
+        //        printPass(Employee.hourlyMaintenance, person: plugHourlyEmployeeMaintenance)
+        //        print("\n")
+        //
+        //        let plugManager = PersonalInfo(firstName: "Ian", lastName: "Chang", dateOfBirth:nil, street: "203 Turtle Road", city: "San Francisco", state: "CA", zip: "93028")
+        //        printPass(Manager.manager, person: plugManager)
+        //        print("\n")
+        //
+        //        let plugChildGuestAgeNotEligible = PersonalInfo(firstName: nil, lastName: nil, dateOfBirth: "03/02/2010", street: nil, city: nil, state: nil, zip: nil)
+        //        printPass(Guest.freeChild, person: plugChildGuestAgeNotEligible)
+        //        print("\n")
+        //
+        //        let plugManagerMissingFirstName = PersonalInfo(firstName: nil, lastName: "Chang", dateOfBirth:nil, street: "203 Turtle Road", city: "San Francisco", state: "CA", zip: "93028")
+        //        printPass(Manager.manager, person: plugManagerMissingFirstName)
+        //        print("\n")
+        //
+        //        let plugManagerMissingLastName = PersonalInfo(firstName: "Ian", lastName: nil, dateOfBirth:nil, street: "203 Turtle Road", city: "San Francisco", state: "CA", zip: "93028")
+        //        printPass(Manager.manager, person: plugManagerMissingLastName)
+        //        print("\n")
+        //
+        //        let plugManagerMissingStreet = PersonalInfo(firstName: "Ian", lastName: "Chang", dateOfBirth:nil, street: nil, city: "San Francisco", state: "CA", zip: "93028")
+        //        printPass(Manager.manager, person: plugManagerMissingStreet)
+        //        print("\n")
+        //
+        //        let plugManagerMissingCity = PersonalInfo(firstName: "Ian", lastName: "Chang", dateOfBirth:nil, street: "203 Turtle Road", city: nil, state: "CA", zip: "93028")
+        //        printPass(Manager.manager, person: plugManagerMissingCity)
+        //        print("\n")
+        //
+        //        let plugManagerMissingState = PersonalInfo(firstName: "Ian", lastName: "Chang", dateOfBirth:nil, street: "203 Turtle Road", city: "San Francisco", state: nil, zip: "93028")
+        //        printPass(Manager.manager, person: plugManagerMissingState)
+        //        print("\n")
+        //
+        //        let plugManagerMissingZip = PersonalInfo(firstName: "Ian", lastName: "Chang", dateOfBirth:nil, street: "203 Turtle Road", city: "San Francisco", state: "CA", zip: nil)
+        //        printPass(Manager.manager, person: plugManagerMissingZip)
+        //        print("\n")
+        
+        // Plug data for swiper methods testing
+        
+        let plugPersonForSwiper = PersonalInfo(firstName: "Charlie", lastName: "Brown", dateOfBirth: "07-04-1986", street: "920 Peanut Street", city: "Chicago", state: "IL", zip: "37283")
+        printPass(Employee.hourlyMaintenance, person: plugPersonForSwiper)
+        print("\n")
+        if let pass = generatePass(Employee.hourlyMaintenance, person: plugPersonForSwiper) {
+            print("Swiper methods tests:")
+            areaAcessSwiper(pass, toTestArea: RestrictedAreas.office)
+            print("\n")
+            rideAccessSwiper(pass)
+            print("\n")
+            discountAccessSwiper(pass)
+        }
     }
 
     override func didReceiveMemoryWarning() {
