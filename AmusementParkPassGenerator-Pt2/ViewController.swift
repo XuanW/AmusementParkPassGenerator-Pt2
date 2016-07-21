34a481d661506d012abdc1b26505f5692708900c
Xuan Wang <wangxuan.buaa@gmail.com>
2016-07-21 15:59:22 -0700

    Initial Commit

---
+++ AmusementParkPassGenerator-Pt2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmusementParkPassGenerator-Pt2
+//
+//  Created by XuanWang on 7/21/16.
+//  Copyright © 2016 XuanWang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
